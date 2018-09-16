ae02011d5a992444c0626a8be7072008ae23a9e2
Mr.wang <jiucheng0605@gmail.com>
2018-09-16 18:33:42 +0800

    简单封装测试服务器接口

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+// 项目配置
+const host = "http://localhost:5757"
+
+const Config = {
+  host,
+}
+
+export default Config
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,24 +1,45 @@
-function formatNumber (n) {
-  const str = n.toString()
-  return str[1] ? str : `0${str}`
+// config
+import Config from "@/config"
+
+function formatNumber(n) {
+	const str = n.toString()
+	return str[1] ? str : `0${str}`
 }
 
-export function formatTime (date) {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
+export function formatTime(date) {
+	const year = date.getFullYear()
+	const month = date.getMonth() + 1
+	const day = date.getDate()
 
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+	const hour = date.getHours()
+	const minute = date.getMinutes()
+	const second = date.getSeconds()
 
-  const t1 = [year, month, day].map(formatNumber).join('/')
-  const t2 = [hour, minute, second].map(formatNumber).join(':')
+	const t1 = [year, month, day].map(formatNumber).join('/')
+	const t2 = [hour, minute, second].map(formatNumber).join(':')
 
-  return `${t1} ${t2}`
+	return `${t1} ${t2}`
 }
+
+
+export function get(url) {
+	return new Promise((resolve, reject) => {
+		wx.request({
+			url: Config.host + url,
+			success: function (res) {
+				if (res.data.code == 0) {
+					resolve(res.data.data)
+				} else {
+					reject(res.data)
+				}
+			}
+		})
+	})
+}
+
 
 export default {
   formatNumber,
-  formatTime
+  formatTime,
+  get
 }
