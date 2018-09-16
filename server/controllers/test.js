b37d607c0f48a034a76f6af4b9e9a71e5dca77c8
Mr.wang <jiucheng0605@gmail.com>
2018-09-16 15:09:26 +0800

    测试开启服务端test

---
+++ server/controllers/test.js
@@ -0,0 +1,6 @@
+// 测试一个数据
+module.exports = async (ctx) => {
+    ctx.state.data={
+        content:"测试的数据字段"
+    }
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -32,4 +32,7 @@ router.get('/message', controllers.message.get)
 // POST 用来处理微信转发过来的客服消息
 router.post('/message', controllers.message.post)
 
+// 添加测试的接口
+router.get('/test', controllers.test)
+
 module.exports = router
